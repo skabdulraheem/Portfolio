4a71352ef0cd422ba4b4ab96d0b817f75c995f25
Raheem <124266599+skabdulraheem@users.noreply.github.com>
2026-04-23 22:36:16 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white">
-      Copyright © 2024 Venkatesh S.All Rights reserved.
+      Copyright © 2024 Shaik Raheem .All Rights reserved.
     </div>
   );
 };
